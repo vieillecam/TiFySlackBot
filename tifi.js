7563c8b0221c0d1f124b7fc9a59601c1d8e14c45
vieillecam <vieillecam@free.fr>
2016-03-16 16:47:42 -0400

    fixed if trigger is not null

--- tifi.js
+++ tifi.js
@@ -16,7 +16,7 @@ module.exports = function (req, res, next) {
   console.log('trigger',trigger);
   console.log('reqText',itemNumber);
 
-  if (trigger.toUpperCase() == "TFS[") {
+  if (trigger && trigger.toUpperCase() == "TFS[") {
     itemNumber = itemNumber.slice(itemNumber.indexOf("[") + 1, itemNumber.indexOf("]"));
     botPayload.text = "Here is your link : " + getLinkFrom(itemNumber);
   }
